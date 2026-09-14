62e3fa09e1eac07d6ec4de447dac72ea00f279bd
chuihuarachif-cpu <chuihuarachif@gmail.com>
2026-09-14 14:06:30 -0400

    style: promote premium release cache key to R198

--- v2/premium-release.js
+++ v2/premium-release.js
@@ -1,8 +1,8 @@
 (() => {
   'use strict';
 
-  const VERSION = 'premium-release-r197-20260914';
-  const RELEASE = '20260914-r197-reference-match';
+  const VERSION = 'premium-release-r198-20260914';
+  const RELEASE = '20260914-r198-reference-match';
   const root = document.documentElement;
   let active = '';
 
